61d5777114812c88982ec901cc89e93d05b1d3e6
dmitrykaliasinski <d.k.akadiman@gmail.com>
2022-02-23 14:30:47 +0300

    change code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let symbol = [];
+  let strArr = str.split("");
+  const open = bracketsConfig.map(([open]) => open);
+  const close = bracketsConfig.map(([, close]) => close);
+  for (let i = 0; i < strArr.length; i++) {
+    if (open.includes(strArr[i])) {
+      symbol.push(strArr[i]);
+    }
+    if (close.includes(strArr[i])) {
+      if (open[close.indexOf(strArr[i])] === symbol[symbol.length - 1]) {
+        symbol.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return symbol.length === 0;
+};
